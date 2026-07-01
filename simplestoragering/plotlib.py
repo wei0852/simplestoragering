5e57ced6cd9640b9f735a4a6720df4631f421fb1
weibf <weibf@ustc.edu.cn>
2026-07-01 11:15:31 +0800

    feat: modified plot functions

--- simplestoragering/plotlib.py
+++ simplestoragering/plotlib.py
@@ -138,6 +138,8 @@ def plot_layout_in_ax(ele_list: list, ax: Axes, ratio=0.03) -> None:
     """Draw the layout in matplotlib.axes.Axes and the layout will appear at the bottom of the figure. ratio controls the height."""
     current_s = 0
     i = 0
+    mb = rb = quad = sext = oct = None
+
     while i < len(ele_list):
         if isinstance(ele_list[i], Quadrupole):
             begin_s = current_s
@@ -146,16 +148,16 @@ def plot_layout_in_ax(ele_list: list, ax: Axes, ratio=0.03) -> None:
             layout_s += [(begin_s + current_s) / 2, current_s, current_s]
             draw_data = ele_list[i].k1 / abs(ele_list[i].k1) if ele_list[i].k1 != 0 else 0
             layout_data = [0, 1, 1 + 0.3 * draw_data, 1, 0]
-            ax.fill(layout_s, layout_data, color='#EE3030')
+            quad, = ax.fill(layout_s, layout_data, color='#EE3030', label='Quadrupoles')
         elif isinstance(ele_list[i], HBend):
             layout_s = [current_s, current_s]
             current_s += ele_list[i].length
             layout_s += [current_s, current_s]
             layout_data = [0, 1, 1, 0]
             if ele_list[i].h >= 0:
-                ax.fill(layout_s, layout_data, color='#3d3dcd')
+                mb, = ax.fill(layout_s, layout_data, color='#3d3dcd', label='Main bends')
             else:
-                ax.fill(layout_s, layout_data, color='#00eeee')
+                rb, = ax.fill(layout_s, layout_data, color="#6ab6fe", label='Reverse bends')
         elif isinstance(ele_list[i], Sextupole):
             begin_s = current_s
             layout_s = [current_s, current_s]
@@ -163,18 +165,19 @@ def plot_layout_in_ax(ele_list: list, ax: Axes, ratio=0.03) -> None:
             layout_s += [(begin_s + current_s) / 2, current_s, current_s]
             draw_data = ele_list[i].k2 / abs(ele_list[i].k2) if ele_list[i].k2 != 0 else 0
             layout_data = [0, 1, 1 + 0.3 * draw_data, 1, 0]
-            ax.fill(layout_s, layout_data, color='#3dcd3d')
+            sext, = ax.fill(layout_s, layout_data, color='#3dcd3d', label='Sextupoles')
         elif isinstance(ele_list[i], Octupole):
             layout_s = [current_s, current_s]
             current_s += ele_list[i].length
             layout_s += [current_s, current_s]
             layout_data = [0, 1, 1, 0]
-            ax.fill(layout_s, layout_data, color='#8B3A3A')
+            oct, = ax.fill(layout_s, layout_data, color='#8B3A3A', label='Octupoles')
         else:
             current_s += ele_list[i].length
         i += 1
     ax.set_ylim(0, 1/ratio)
     ax.set_yticks([])
+    return [h for h in [mb, rb, quad, sext, oct] if h is not None]
 
 
 def plot_with_background(s, y: dict, lattice):
@@ -190,74 +193,99 @@ def plot_with_background(s, y: dict, lattice):
     plt.show()
 
 
-def plot_resonance_line_in_ax(ax: Axes, order: int = 3, refnux: float=None, refnuy: float=None):
+def plot_resonance_line_in_ax(ax: Axes, order: int = 3, refnux: float=None, refnuy: float=None, number_sp: int = 1):
     """plot the resonance lines in matplotlib.axes.Axes, the plot area is [int(refnux), 1 + int(refnux)] x [int(refnuy), 1 + int(refnuy)]"""
 
-    third_color = '#808080'  #
-    fourth_color = '#a0a0a0'  #
-    fifth_color = '#dddddd'  #
+    Qx_int = 0 if refnux is None else int(refnux)
+    Qy_int = 0 if refnuy is None else int(refnuy)
+    if number_sp == 1:
+        first_color = '#000000'
+        second_color = '#202020'
+        third_color = '#808080'  #
+        fourth_color = '#a0a0a0'  #
+        fifth_color = '#dddddd'  #
+        linestyle = (0, (1, 1))
+        first_width = 3
+    else:
+        first_width = 6
+        first_color = '#ff0000'
+        second_color = '#ff2020'
+        third_color = '#ff8080'  #
+        fourth_color = '#ffa0a0'  #
+        fifth_color = '#ffdddd'  #
+        linestyle = '-'
+        Qx_int = Qx_int - Qx_int % number_sp
+        Qy_int = Qy_int - Qy_int % number_sp
+    second_width = 2
     third_width = 1.8
     fourth_width = 1
     fifth_width = 0.8
-    Qx_int = 0 if refnux is None else int(refnux)
-    Qy_int = 0 if refnuy is None else int(refnuy)
     order = 3 if order < 3 else order
-    line1, = ax.plot([Qx_int + 1, Qx_int + 1], [Qy_int, Qy_int + 1], c='#000000', linewidth=3, label='1')
-    ax.plot([Qx_int, Qx_int], [Qy_int, Qy_int + 1], c='#000000', linewidth=6)
-    ax.plot([Qx_int, Qx_int + 1], [Qy_int, Qy_int], c='#000000', linewidth=6)
-    ax.plot([Qx_int, Qx_int + 1], [Qy_int + 1, Qy_int + 1], c='#000000', linewidth=6)
+    line1, = ax.plot([Qx_int + number_sp, Qx_int + number_sp], [Qy_int, Qy_int + number_sp], c=first_color, linewidth=first_width, label='1', linestyle=linestyle)
+    ax.plot([Qx_int, Qx_int], [Qy_int, Qy_int + number_sp], c=first_color, linewidth=first_width, linestyle=linestyle)
+    ax.plot([Qx_int, Qx_int + number_sp], [Qy_int, Qy_int], c=first_color, linewidth=first_width, linestyle=linestyle)
+    ax.plot([Qx_int, Qx_int + number_sp], [Qy_int + number_sp, Qy_int + number_sp], c=first_color, linewidth=first_width, linestyle=linestyle)
     # 2nd black
-    line2, = ax.plot([0.5 + Qx_int, 0.5 + Qx_int], [0. + Qy_int, 1 + Qy_int], c='#202020', linewidth=1.8, label='2')  # 2 nuy
-    ax.plot([0. + Qx_int, 1 + Qx_int], [0.5 + Qy_int, 0.5 + Qy_int], c='#202020', linewidth=1.8)  # 2 nux
+    line2, = ax.plot([0.5 * number_sp + Qx_int, 0.5 * number_sp + Qx_int], [0. + Qy_int, number_sp + Qy_int], c=second_color, linewidth=second_width, linestyle=linestyle, label='2')  # 2 nuy
+    ax.plot([0. + Qx_int, number_sp + Qx_int], [0.5 * number_sp + Qy_int, 0.5 * number_sp + Qy_int], c=second_color, linewidth=second_width, linestyle=linestyle)  # 2 nux
+    ax.plot([0. + Qx_int, number_sp + Qx_int], [1 * number_sp + Qy_int, 0. * number_sp + Qy_int], c=second_color, linewidth=second_width, linestyle=linestyle)   # 2 nux
+    ax.plot([0. + Qx_int, number_sp + Qx_int], [0. * number_sp + Qy_int, 1 * number_sp + Qy_int], c=second_color, linewidth=second_width, linestyle=linestyle)   # 2 nux
     # 3rd red
-    line3, = ax.plot([0. + Qx_int, 1.0 + Qx_int], [0.5 + Qy_int, 1.0 + Qy_int], c=third_color, linewidth=third_width, label='3')  # nux - 2 nuy
-    ax.plot([2 / 3 + Qx_int, 2 / 3 + Qx_int], [0 + Qy_int, 1 + Qy_int], c=third_color, linewidth=third_width)  # 3 nux
-    ax.plot([1 / 3 + Qx_int, 1 / 3 + Qx_int], [0 + Qy_int, 1 + Qy_int], c=third_color, linewidth=third_width)  # 3 nuy
-    ax.plot([0. + Qx_int, 1.0 + Qx_int], [1 + Qy_int, 0.5 + Qy_int], c=third_color, linewidth=third_width)  # nux + 2 nuy
-    ax.plot([0. + Qx_int, 1.0 + Qx_int], [0. + Qy_int, 0.5 + Qy_int], c=third_color, linewidth=third_width)  # nux - 2 nuy
-    ax.plot([0. + Qx_int, 1.0 + Qx_int], [0.5 + Qy_int, 0.0 + Qy_int], c=third_color, linewidth=third_width)  # nux + 2 nuy
+    line3, = ax.plot([0. + Qx_int, 1.0 * number_sp + Qx_int], [0.5 * number_sp + Qy_int, 1.0 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle, label='3')  # nux - 2 nuy
+    ax.plot([2 / 3 * number_sp + Qx_int, 2 / 3 * number_sp + Qx_int], [0 + Qy_int, 1 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # 3 nux
+    ax.plot([1 / 3 * number_sp + Qx_int, 1 / 3 * number_sp + Qx_int], [0 + Qy_int, 1 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # 3 nuy
+    ax.plot([0. * number_sp + Qx_int, 1. * number_sp + Qx_int], [1/3 * number_sp + Qy_int, 1/3 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # 3 nuy
+    ax.plot([0. * number_sp + Qx_int, 1. * number_sp + Qx_int], [2/3 * number_sp + Qy_int, 2/3 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # 3 nuy
+    ax.plot([0. * number_sp + Qx_int, 1.0 * number_sp + Qx_int], [1 * number_sp + Qy_int, 0.5 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # nux + 2 nuy
+    ax.plot([0. * number_sp + Qx_int, 1.0 * number_sp + Qx_int], [0. + Qy_int, 0.5 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # nux - 2 nuy
+    ax.plot([0. * number_sp + Qx_int, 1.0 * number_sp + Qx_int], [0.5 * number_sp + Qy_int, 0.0 + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # nux + 2 nuy
+    ax.plot([0. * number_sp + Qx_int, 0.5 * number_sp + Qx_int], [0. * number_sp + Qy_int, 1.0 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # 2nux + nuy
+    ax.plot([0.5 * number_sp + Qx_int, 1.0 * number_sp + Qx_int], [0. * number_sp + Qy_int, 1.0 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # 2nux + nuy
+    ax.plot([0.5 * number_sp + Qx_int, 0.0 * number_sp + Qx_int], [0. * number_sp + Qy_int, 1.0 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # 2nux + nuy
+    ax.plot([1.0 * number_sp + Qx_int, 0.5 * number_sp + Qx_int], [0. * number_sp + Qy_int, 1.0 * number_sp + Qy_int], c=third_color, linewidth=third_width, linestyle=linestyle)  # 2nux + nuy
     lines = [line1, line2, line3]
     if order >= 4:
         # 4th
-        line4, = ax.plot([0.5 + Qx_int, 1.0 + Qx_int], [1 + Qy_int, 0.50 + Qy_int], c=fourth_color, linewidth=fourth_width, label='4')  # 2 nux + 2 nuy
+        line4, = ax.plot([0.5 * number_sp + Qx_int, 1.0 * number_sp + Qx_int], [1 * number_sp + Qy_int, 0.50 * number_sp + Qy_int], c=fourth_color, linewidth=fourth_width, linestyle=linestyle, label='4')  # 2 nux + 2 nuy
+        ax.plot([0.   * number_sp + Qx_int, 1.0  * number_sp+ Qx_int], [0  * number_sp+ Qy_int, 1 * number_sp + Qy_int], c=fourth_color, linewidth=fourth_width, linestyle=linestyle)  # 2 nux - 2 nuy
+        ax.plot([0.   * number_sp + Qx_int, 1.0  * number_sp+ Qx_int], [1  * number_sp+ Qy_int, 0.0 * number_sp + Qy_int], c=fourth_color, linewidth=fourth_width, linestyle=linestyle)  # 2 nux + 2 nuy
+        ax.plot([0.5  * number_sp + Qx_int, 1.0  * number_sp+ Qx_int], [0  * number_sp+ Qy_int, 0.50 * number_sp + Qy_int], c=fourth_color, linewidth=fourth_width, linestyle=linestyle)  # 2 nux - 2 nuy
+        ax.plot([0.   * number_sp + Qx_int, 0.5  * number_sp+ Qx_int], [0.5  * number_sp+ Qy_int, 0.0 * number_sp + Qy_int], c=fourth_color, linewidth=fourth_width, linestyle=linestyle)  # 2 nux + 2 nuy
+        ax.plot([0.   * number_sp + Qx_int, 0.5  * number_sp+ Qx_int], [0.5  * number_sp+ Qy_int, 1.0 * number_sp + Qy_int], c=fourth_color, linewidth=fourth_width, linestyle=linestyle)  # 2 nux - 2 nuy
+        ax.plot([0.   * number_sp + Qx_int, 1    * number_sp+ Qx_int], [0.25  * number_sp+ Qy_int, 0.250 * number_sp + Qy_int], c=fourth_color, linewidth=fourth_width, linestyle=linestyle)
+        ax.plot([0.   * number_sp + Qx_int, 1    * number_sp+ Qx_int], [0.75  * number_sp+ Qy_int, 0.750 * number_sp + Qy_int], c=fourth_color, linewidth=fourth_width, linestyle=linestyle)
+        ax.plot([0.25 * number_sp + Qx_int, 0.25 * number_sp + Qx_int], [0.  * number_sp+ Qy_int, 1.0 * number_sp + Qy_int], c=fourth_color, linewidth=fourth_width, linestyle=linestyle)
+        ax.plot([0.75 * number_sp + Qx_int, 0.75 * number_sp + Qx_int], [0.  * number_sp+ Qy_int, 1.0 * number_sp + Qy_int], c=fourth_color, linewidth=fourth_width, linestyle=linestyle)
         lines.append(line4)
-        ax.plot([0.   + Qx_int, 1.0 + Qx_int], [0 + Qy_int, 1 + Qy_int], c=fourth_color, linewidth=fourth_width)  # 2 nux - 2 nuy
-        ax.plot([0.   + Qx_int, 1.0 + Qx_int], [1 + Qy_int, 0.0 + Qy_int], c=fourth_color, linewidth=fourth_width)  # 2 nux + 2 nuy
-        ax.plot([0.5  + Qx_int, 1.0 + Qx_int], [0 + Qy_int, 0.50 + Qy_int], c=fourth_color, linewidth=fourth_width)  # 2 nux - 2 nuy
-        ax.plot([0.   + Qx_int, 0.5 + Qx_int], [0.5 + Qy_int, 0.0 + Qy_int], c=fourth_color, linewidth=fourth_width)  # 2 nux + 2 nuy
-        ax.plot([0.   + Qx_int, 0.5 + Qx_int], [0.5 + Qy_int, 1.0 + Qy_int], c=fourth_color, linewidth=fourth_width)  # 2 nux - 2 nuy
-        ax.plot([0.   + Qx_int, 1   + Qx_int], [0.25 + Qy_int, 0.250 + Qy_int], c=fourth_color, linewidth=fourth_width)
-        ax.plot([0.   + Qx_int, 1   + Qx_int], [0.75 + Qy_int, 0.750 + Qy_int], c=fourth_color, linewidth=fourth_width)
-        ax.plot([0.25 + Qx_int, 0.25 + Qx_int], [0. + Qy_int, 1.0 + Qy_int], c=fourth_color, linewidth=fourth_width)
-        ax.plot([0.75 + Qx_int, 0.75 + Qx_int], [0. + Qy_int, 1.0 + Qy_int], c=fourth_color, linewidth=fourth_width)
     if order >= 5:
         # 5th
-        line5, = ax.plot([0 + Qx_int, 2 / 3 + Qx_int], [0 + Qy_int, 1 + Qy_int], c=fifth_color, linewidth=fifth_width, label='5')  # 2 nux - 3 nuy
+        line5, = ax.plot([0 * number_sp + Qx_int, 2 / 3 * number_sp + Qx_int], [0 * number_sp + Qy_int, 1 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width, label='5')  # 2 nux - 3 nuy
+        ax.plot([0 * number_sp + Qx_int, 1 * number_sp + Qx_int], [0 * number_sp + Qy_int, 0.25 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)  # nux - 4 nuy
+        ax.plot([0 * number_sp + Qx_int, 1 * number_sp + Qx_int], [0.25 * number_sp + Qy_int, 0 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)  # nux + 4 nuy
+        ax.plot([0 * number_sp + Qx_int, 1 / 3 * number_sp + Qx_int], [0.5 * number_sp + Qy_int, 0 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)  #
+        ax.plot([0 * number_sp + Qx_int, 1 * number_sp + Qx_int], [0.5 * number_sp + Qy_int, 0.25 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)  #
+        ax.plot([0 * number_sp + Qx_int, 1 / 3 * number_sp + Qx_int], [0.5 * number_sp + Qy_int, 1 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)
+        ax.plot([0 * number_sp + Qx_int, 1 * number_sp + Qx_int], [0.5 * number_sp + Qy_int, 0.75 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)  #
+        ax.plot([0 * number_sp + Qx_int, 1 * number_sp + Qx_int], [0.25 * number_sp + Qy_int, 0.5 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)  #
+        ax.plot([0 * number_sp + Qx_int, 1 * number_sp + Qx_int], [0.75 * number_sp + Qy_int, 0.5 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)  #
+        ax.plot([0 * number_sp + Qx_int, 1 * number_sp + Qx_int], [0.75 * number_sp + Qy_int, 1 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)  #
+        ax.plot([0 * number_sp + Qx_int, 1 * number_sp + Qx_int], [1 * number_sp + Qy_int, 0.75 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)  #
+        ax.plot([0.2 * number_sp + Qx_int, 0.2 * number_sp + Qx_int], [0. * number_sp + Qy_int, 1.0 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)
+        ax.plot([0.4 * number_sp + Qx_int, 0.4 * number_sp + Qx_int], [0. * number_sp + Qy_int, 1.0 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)
+        ax.plot([0.6 * number_sp + Qx_int, 0.6 * number_sp + Qx_int], [0. * number_sp + Qy_int, 1.0 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)
+        ax.plot([0.8 * number_sp + Qx_int, 0.8 * number_sp + Qx_int], [0. * number_sp + Qy_int, 1.0 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)
+        ax.plot([1 / 3 * number_sp + Qx_int, 1 * number_sp + Qx_int], [0. * number_sp + Qy_int, 1.0 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)
+        ax.plot([0. * number_sp + Qx_int, 2 / 3 * number_sp + Qx_int], [1. * number_sp + Qy_int, .0 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)
+        ax.plot([2 / 3 * number_sp + Qx_int, 1 * number_sp + Qx_int], [0. * number_sp + Qy_int, 0.50 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)
+        ax.plot([1 / 3 * number_sp + Qx_int, 1 * number_sp + Qx_int], [1. * number_sp + Qy_int, .0 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)
+        ax.plot([2 / 3 * number_sp + Qx_int, 1 * number_sp + Qx_int], [1. * number_sp + Qy_int, 0.50 * number_sp + Qy_int], c=fifth_color, linewidth=fifth_width)
         lines.append(line5)
-        ax.plot([0 + Qx_int, 1 + Qx_int], [0 + Qy_int, 0.25 + Qy_int], c=fifth_color, linewidth=fifth_width)  # nux - 4 nuy
-        ax.plot([0 + Qx_int, 1 + Qx_int], [0.25 + Qy_int, 0 + Qy_int], c=fifth_color, linewidth=fifth_width)  # nux + 4 nuy
-        ax.plot([0 + Qx_int, 1 / 3 + Qx_int], [0.5 + Qy_int, 0 + Qy_int], c=fifth_color, linewidth=fifth_width)  #
-        ax.plot([0 + Qx_int, 1 + Qx_int], [0.5 + Qy_int, 0.25 + Qy_int], c=fifth_color, linewidth=fifth_width)  #
-        ax.plot([0 + Qx_int, 1 / 3 + Qx_int], [0.5 + Qy_int, 1 + Qy_int], c=fifth_color, linewidth=fifth_width)
-        ax.plot([0 + Qx_int, 1 + Qx_int], [0.5 + Qy_int, 0.75 + Qy_int], c=fifth_color, linewidth=fifth_width)  #
-        ax.plot([0 + Qx_int, 1 + Qx_int], [0.25 + Qy_int, 0.5 + Qy_int], c=fifth_color, linewidth=fifth_width)  #
-        ax.plot([0 + Qx_int, 1 + Qx_int], [0.75 + Qy_int, 0.5 + Qy_int], c=fifth_color, linewidth=fifth_width)  #
-        ax.plot([0 + Qx_int, 1 + Qx_int], [0.75 + Qy_int, 1 + Qy_int], c=fifth_color, linewidth=fifth_width)  #
-        ax.plot([0 + Qx_int, 1 + Qx_int], [1 + Qy_int, 0.75 + Qy_int], c=fifth_color, linewidth=fifth_width)  #
-        ax.plot([0.2 + Qx_int, 0.2 + Qx_int], [0. + Qy_int, 1.0 + Qy_int], c=fifth_color, linewidth=fifth_width)
-        ax.plot([0.4 + Qx_int, 0.4 + Qx_int], [0. + Qy_int, 1.0 + Qy_int], c=fifth_color, linewidth=fifth_width)
-        ax.plot([0.6 + Qx_int, 0.6 + Qx_int], [0. + Qy_int, 1.0 + Qy_int], c=fifth_color, linewidth=fifth_width)
-        ax.plot([0.8 + Qx_int, 0.8 + Qx_int], [0. + Qy_int, 1.0 + Qy_int], c=fifth_color, linewidth=fifth_width)
-        ax.plot([1 / 3 + Qx_int, 1 + Qx_int], [0. + Qy_int, 1.0 + Qy_int], c=fifth_color, linewidth=fifth_width)
-        ax.plot([0. + Qx_int, 2 / 3 + Qx_int], [1. + Qy_int, .0 + Qy_int], c=fifth_color, linewidth=fifth_width)
-        ax.plot([2 / 3 + Qx_int, 1 + Qx_int], [0. + Qy_int, 0.50 + Qy_int], c=fifth_color, linewidth=fifth_width)
-        ax.plot([1 / 3 + Qx_int, 1 + Qx_int], [1. + Qy_int, .0 + Qy_int], c=fifth_color, linewidth=fifth_width)
-        ax.plot([2 / 3 + Qx_int, 1 + Qx_int], [1. + Qy_int, 0.50 + Qy_int], c=fifth_color, linewidth=fifth_width)
-    ax.set_xlim(Qx_int, Qx_int + 1.2)
-    ax.set_xticks([Qx_int, Qx_int + 0.2, Qx_int + 0.4, Qx_int + 0.6, Qx_int + 0.8, Qx_int + 1])
-    ax.set_ylim(Qy_int, Qy_int + 1)
-    line_legend = ax.legend(handles=lines, loc='lower right')
-    ax.add_artist(line_legend)
+    # ax.set_xlim(Qx_int, Qx_int + 1.2)
+    # ax.set_xticks([Qx_int, Qx_int + 0.2, Qx_int + 0.4, Qx_int + 0.6, Qx_int + 0.8, Qx_int + 1])
+    # ax.set_ylim(Qy_int, Qy_int + 1)
+    # line_legend = ax.legend(handles=lines, loc='lower right')
+    # ax.add_artist(line_legend)
+    return lines
 
 
 def plot_RDTs_along_ring(lattice: CSLattice, RDT_type='f', show=True) -> dict[np.ndarray]:
